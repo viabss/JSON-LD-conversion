c501c926062b6ad175b4adf4067e5a822acf0d86
Saurav Bhattacharyya <saurav.bhattacharyya@gmail.com>
2017-10-10 21:44:12 -0400

    WIP-2 with prefLabel Dictionary created

--- Alpha_JSONLD.py
+++ Alpha_JSONLD.py
@@ -4,11 +4,7 @@ def altLabelDict(raw_Data = pd.DataFrame()):
     debug=False
 #    DO NOT USE .fromkeys to initialize the dictionary as it creates one object for all values! In essence
 #    the empty list that we create for all keys is actually shared by them all.
-    
-    key = 'altlabel'
-    altlabel_dict = dict.fromkeys(key)
-    print(altlabel_dict)
-    altlabel_Key = {k:[altlabel_dict] for k in raw_Data['prefLabel']} #The dictionary to store the altLabels for each prefLabel
+    altlabel_Key = {k:[] for k in raw_Data['prefLabel']} #The dictionary to store the altLabels for each prefLabel
     for key,values in altlabel_Key.items():
         for rows in raw_Data.itertuples(index=False):
             if rows.prefLabel in key:
@@ -19,10 +15,20 @@ def altLabelDict(raw_Data = pd.DataFrame()):
         print('[DEBUG]\n')
         print(altlabel_Key)
     return altlabel_Key
-            
-#def prefLabelDict(raw_Data = pd.DataFrame()):
-#    debug = False
-#    preflabel_Key = {k:[] for k in raw_Data['prefLabel']}
+
+           
+def prefLabelDict(raw_Data = pd.DataFrame()):
+    debug = False
+    dedup_raw_Data = raw_Data.drop_duplicates(subset = 'prefLabel',keep = 'first',inplace = False)
+    preflabel_Key = {k:[] for k in dedup_raw_Data['prefLabel']}
+    for key,values in preflabel_Key.items():
+        for rows in dedup_raw_Data.itertuples(index=False):
+            if rows.prefLabel in key:
+                preflabel_Key[key] = list(rows)
+    if debug:
+        print('[DEBUG]\n')
+        print(preflabel_Key)
+    return preflabel_Key
     
                
     
@@ -31,7 +37,8 @@ def altLabelDict(raw_Data = pd.DataFrame()):
 def import_file():
     debug=False
     altlabel_Dict = {}
-    path = 'C:/Users/viabs/Documents/Apps/JSON-LD' 
+    preflabel_Dict = {}
+    path = 'C:/' 
     file = 'Normalized_TEST_python.xlsx' 
     full_Path = path+'/'+file
     raw_Data=pd.DataFrame()
@@ -39,8 +46,9 @@ def import_file():
     if debug:
         print('[DEBUG]\n')
         print(raw_Data[['prefLabel','altLabel']])
-    altlabel_Dict=altLabelDict(raw_Data[['prefLabel','altLabel']])
-#    prefLabelDict(raw_Data)
+    altlabel_Dict = altLabelDict(raw_Data[['prefLabel','altLabel']])
+    preflabel_Dict = prefLabelDict(raw_Data.drop('altLabel',axis = 1))
+    return altlabel_Dict,preflabel_Dict
     
 
 def main():
