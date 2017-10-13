d1ecccf0567409abe1a3f6cc552d59f8973fa4d8
Saurav Bhattacharyya <saurav.bhattacharyya@gmail.com>
2017-10-12 20:14:31 -0400

    with write function

--- Alpha_JSONLD.py
+++ Alpha_JSONLD.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import json
 debug=True
 id_Seed = 1
+
+path = 'C:\\Users\\viabs\\Documents\\Apps\\JSON-LD-conversion\\' 
+file = 'Normalized TEST_python.xlsx' 
+
 def altLabelDict(raw_Data = pd.DataFrame()):
     debug=False
 #    DO NOT USE .fromkeys to initialize the dictionary as it creates one object for all values! In essence
@@ -9,7 +14,7 @@ def altLabelDict(raw_Data = pd.DataFrame()):
     for key,values in altlabel_Key.items():
         for rows in raw_Data.itertuples(index=False):
             if rows.prefLabel in key:
-                altlabel_Key[key].append(rows.altLabel.strip())
+                altlabel_Key[key].append('"{}"'.format(rows.altLabel.strip()))
             else:
                 continue
     if debug:
@@ -19,27 +24,26 @@ def altLabelDict(raw_Data = pd.DataFrame()):
                
 
 def create_JSONLD(altlabel_Dict={}):
-    debug = True
+    debug = False
     global id_Seed 
     norm_JSON_record = {} #The final JSON-LD dictionary for each record
     norm_JSON_list = [] #The list of all records in the norm_JSON
     norm_JSON={}
     for key,values in altlabel_Dict.items():
         id_string = ('00000000'+str(id_Seed))[len('00000000'+str(id_Seed))-9:]
-        keys = ["@context","@type","@id","additionaltype","prefLabel","altLabel"] 
-        values = ['http://schema.org','Corporation','MFG'+id_string,'Manufacturer',key,values]
+        keys = ['"{}"'.format('@context'),'"{}"'.format('@type'),'"{}"'.format('@id'),'"{}"'.format('additionaltype'),'"{}"'.format('prefLabel'),'"{}"'.format('altLabel')] 
+        values = ['"{}"'.format('http://schema.org'),'"{}"'.format('Corporation'),'"{}"'.format('MFG'+id_string),'"{}"'.format('Manufacturer'),key,values]
         norm_JSON_record = {key:values for key,values in zip(keys,values)}
         norm_JSON_list.append(norm_JSON_record)
         id_Seed+=1
-    norm_JSON['corporations']= norm_JSON_list
+    norm_JSON['"{}"'.format('corporations')]= norm_JSON_list
     if debug:
         print('[DEBUG]\n')
         print(norm_JSON)
+    return(norm_JSON)
 
 def import_file():
     debug=False
-    path = 'C:\\Users\\saurav.bhattacharyya\\Documents\\' 
-    file = 'Normalized TEST_python.xlsx' 
     full_Path = path+file
     raw_Data=pd.DataFrame()
     try:
@@ -51,14 +55,23 @@ def import_file():
     except IOError:
         print("File does not exist in the location")
         return 0
-        
+    
+def export_file(norm_JSON = {}):
+    with open(file,'w') as outfile:
+        json.dump(norm_JSON,outfile)
+    
+    
+    
+       
 
 def main():
     raw_Data = pd.DataFrame()
-    altlabel_Dict = {}   
+    altlabel_Dict = {}
+    norm_JSON = {}
     raw_Data = import_file()
     altlabel_Dict = altLabelDict(raw_Data[['prefLabel','altLabel']])
-    create_JSONLD(altlabel_Dict)
+    norm_JSON = create_JSONLD(altlabel_Dict)
+    export_file(norm_JSON)
     
     
 main()
