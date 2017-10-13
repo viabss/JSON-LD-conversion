1c1d82e87a23e36c0a79d2dab6607a2538bfba4d
Saurav Bhattacharyya <saurav.bhattacharyya@construction.com>
2017-10-13 15:03:39 -0400

    Version 1.00. It will read an excel file of a pre-determined structure, convert it to a corporation schema JSON-LD and save it to a file in a pre-determined folder

--- Alpha_JSONLD.py
+++ Alpha_JSONLD.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import json
+import os
 debug=True
-id_Seed = 1
 
-path_in = 'I:\\General\\Normalized Files\\Normalized Input\\' 
-file = 'Manufacturer Normalization.xlsx' 
-path_out = path_in.replace('Input','Output')
+drive = 'I:\\'
+subFolder_1 = 'General'
+subFolder_2 = 'Normalized Files'
+subFolder_3_input = 'Normalized Input'
+subFolder_3_output = 'Normalized Output'
 
 def altLabelDict(raw_Data = pd.DataFrame()):
     debug=False
@@ -24,15 +26,14 @@ def altLabelDict(raw_Data = pd.DataFrame()):
     return altlabel_Key
                
 
-def create_JSONLD(altlabel_Dict={}):
+def create_JSONLD(id_Seed,altlabel_Dict={}):
     debug = False
-    global id_Seed 
     norm_JSON_record = {} #The final JSON-LD dictionary for each record
     norm_JSON_list = [] #The list of all records in the norm_JSON
     norm_JSON={}
     for key,values in altlabel_Dict.items():
-        id_string = ('00000000'+str(id_Seed))[len('00000000'+str(id_Seed))-9:]
-        keys = ['@context','@type','@id','additionaltype','prefLabel','altLabel'] 
+        id_string = ('00000000'+str(id_Seed))[len('00000000'+str(id_Seed))-9:] #The ID should be of the form MFGxxxxxxxxx
+        keys = ['@context','@type','@id','additionalType','prefLabel','altLabels'] 
         values = ['http://schema.org','Corporation','MFG'+id_string,'Manufacturer',key,values]
         norm_JSON_record = {key:values for key,values in zip(keys,values)}
         norm_JSON_list.append(norm_JSON_record)
@@ -45,10 +46,11 @@ def create_JSONLD(altlabel_Dict={}):
 
 def import_file(folder):
     debug=False
-    full_Path = path_in+'\\'+folder+'\\'+file
+    file_in = 'Manufacturer Normalization.xlsx'
+    full_path = os.path.join(drive,subFolder_1,subFolder_2,subFolder_3_input,folder,file_in)
     raw_Data=pd.DataFrame()
     try:
-        raw_Data = pd.read_excel(full_Path,header = 0,sheetname='Raw Data',parse_cols = [1,2,3,4],names = ('altLabel','prefLabel','IsNormalized','IsCorrect'))
+        raw_Data = pd.read_excel(full_path,header = 0,sheetname='Raw Data',parse_cols = [1,2,3,4],names = ('altLabel','prefLabel','IsNormalized','IsCorrect'))
         if debug:
             print('[DEBUG]\n')
             print(raw_Data.loc[(raw_Data['IsCorrect']==False)&(raw_Data['IsNormalized']=='Y')])
@@ -58,10 +60,15 @@ def import_file(folder):
         return 0
     
 def export_file(norm_JSON,folder):
-    file = 'norm_JSON.json'
-    full_path = path_out+'\\'+folder+'\\'+file
-    with open(full_path ,'w') as outfile:
-        json.dump(norm_JSON,outfile,indent=4)
+    file_out = 'norm_JSON.json'
+    full_path = os.path.join(drive,subFolder_1,subFolder_2,subFolder_3_output,folder,file_out)
+    path_dir = os.path.join(drive,subFolder_1,subFolder_2,subFolder_3_output,folder)
+    if not os.path.exists(path_dir):
+        os.makedirs(path_dir)
+    with open(full_path,'w+') as file:
+        json.dump(norm_JSON,file,indent=4)
+
+
         
 def main():
     raw_Data = pd.DataFrame()
@@ -69,8 +76,9 @@ def main():
     norm_JSON = {}
     folder = input("Please enter the file Date Stamp of the form [YYYYDDMM]:")
     raw_Data = import_file(folder)
+    id_Seed = int(input('Please enter the seed for the ID(+1 to the last ID):'))
     altlabel_Dict = altLabelDict(raw_Data[['prefLabel','altLabel']])
-    norm_JSON = create_JSONLD(altlabel_Dict)
+    norm_JSON = create_JSONLD(id_Seed,altlabel_Dict)
     export_file(norm_JSON,folder)
     
     
